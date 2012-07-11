1acf0093129e4712c51577005aaaa4d616705356
Michel Fleur <m.fleur@topdesk.com>
2012-07-11 17:56:13 +0200

    Added dependency to guava-testlib and prepared rudimentary tests

---
+++ src/test/java/org/emmef/cheapsets/CheapSetTest.java
@@ -0,0 +1,9 @@
+package org.emmef.cheapsets;
+
+
+public class CheapSetTest {
+//	public static <V> void main(String[] args) {
+//		SetTestSuiteBuilder.using(new TestSetGenerator<>() {
+//		}).
+//	}
+}
---
+++ src/test/java/org/emmef/cheapsets/CheapSetTestBuilder.java
@@ -0,0 +1,7 @@
+package org.emmef.cheapsets;
+
+import com.google.common.collect.testing.SetTestSuiteBuilder;
+
+public class CheapSetTestBuilder<T extends CheapSetTestBuilder<?>> extends SetTestSuiteBuilder<T> {
+
+}
