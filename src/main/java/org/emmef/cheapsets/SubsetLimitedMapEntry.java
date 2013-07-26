cc9e21e9d7d3e03eca7ce24c2301970f628f7f8b
Michel Fleur <m.fleur@topdesk.com>
2013-07-26 10:38:35 +0200

    Removed AbstractEntry as it was only implemented by SubSetLimitedMapEntry. Moved JavaDoc and Map.Entry compliant hashCode/equals to the latter.

--- src/main/java/org/emmef/cheapsets/SubsetLimitedMapEntry.java
+++ src/main/java/org/emmef/cheapsets/SubsetLimitedMapEntry.java
@@ -1,6 +1,18 @@
 package org.emmef.cheapsets;
 
-final class SubsetLimitedMapEntry<K, V> extends AbstractEntry<K, V> {
+import java.util.Map.Entry;
+
+import com.google.common.base.Objects;
+
+/**
+ * Entry object for the {@link SubSetLimitedMap}, that implements
+ * {@link #equals(Object)}, {@link #hashCode()} and {@link #toString()} contract
+ * according to the specifications in {@link Entry}.
+ * 
+ * @see Entry
+ * @see SubSetLimitedMap
+ */
+final class SubsetLimitedMapEntry<K, V> implements Entry<K, V> {
 	private final int index;
 	private final SubSetLimitedMap<K, V> map;
 
@@ -22,5 +34,44 @@ final class SubsetLimitedMapEntry<K, V> extends AbstractEntry<K, V> {
 	@Override
 	public V setValue(V value) {
 		return map.setAt(index, value);
+	}
+	
+	/**
+	 * @see Entry#hashCode()
+	 */
+	@Override
+	public final int hashCode() {
+		Object key = getKey(); 
+		Object value = getValue();
+		
+		return (key != null ? key.hashCode() : 0) ^ (value != null ? value.hashCode() : 0); 
+	};
+	
+	/**
+	 * @see Entry#equals(Object)
+	 */
+	@Override
+	public final boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		
+		if (!(obj instanceof Entry)) {
+			return false;
+		}
+		
+		Entry<?,?> entry = (Entry<?,?>)obj;
+		
+		return Objects.equal(getKey(), entry.getKey()) && Objects.equal(getValue(), entry.getValue());
+	}
+	
+	
+	@Override
+	public final String toString() {
+		K key = getKey();
+		if (key == null) {
+			return "null=" + getValue();
+		}
+		return key.toString() + "=" + getValue();
 	}
 }
