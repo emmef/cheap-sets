d853c95244225c213385bb36ec1f323b317b62eb
Michel Fleur <m.fleur@topdesk.com>
2013-07-26 11:24:11 +0200

    Documented IndexFunction interface

--- src/main/java/org/emmef/cheapsets/IndexFunction.java
+++ src/main/java/org/emmef/cheapsets/IndexFunction.java
@@ -1,16 +1,27 @@
 package org.emmef.cheapsets;
 
+/**
+ * Describes a unique index for a universe of objects.
+ * <p>
+ * Objects that are not in the universe, should have an invalid 
+ * index of {@code -1}.
+ */
 public interface IndexFunction {
 	/**
-	 * Returns the index of the element, which lies between {@code 0} and {@link #indexSize()} {@code - 1}.
+	 * Returns the unique index of the element, which lies between {@code 0} and
+	 * {@link #indexSize()} {@code - 1}.
+	 * 
 	 * @param element element to search index for
-	 * @return the index of the element, which lies between {@code 0} and {@link #indexSize()} {@code - 1}. 
+	 * @return the index of the element, which lies between {@code 0} and
+	 *         {@link #indexSize()} {@code - 1}.
+	 * @documented 2013-07-26
 	 */
 	int indexOf(Object element);
 	
 	/**
 	 * Returns the size of the index.
 	 * @return a positive integer
+	 * @documented 2013-07-26
 	 */
 	int indexSize();
 }
